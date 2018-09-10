eb7ee1da4c571c6493934b40197eafcde4af9b71
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2018-09-10 14:08:03 +0900

    fix redirect

--- dotfiles-isucon/setup.sh
+++ dotfiles-isucon/setup.sh
@@ -19,7 +19,7 @@ fi
 if [ ! -e $HOME/.ssh/github_rsa_isucon ]; then
   cd $HOME/.ssh
   ssh-keygen -t rsa -f github_rsa_isucon -P ""
-  cat << EOT > $HOME/.ssh/config
+  cat << EOT >> $HOME/.ssh/config
 Host github github.com
   HostName github.com
   IdentityFile ~/.ssh/github_rsa_isucon
